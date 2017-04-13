54fa9a2bfaf9ad333d257d7fe9bd8f85acfcfce8
Lorna Jane Mitchell <lorna@lornajane.net>
2017-04-13 10:48:30 +0100

    Missed update to rules, committing it now

--- rules.sh
+++ rules.sh
@@ -3,18 +3,14 @@
 # This sets up the rules and triggers to get our periodic triggers working
 
 wsk trigger delete so_couchdbish
-wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron "*/5 * * * *" --param trigger_payload "{\"tags\": [\"cloudant\",\"ibm-cloudant\",\"couchdb\"]}"
+wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron "*/5 * * * *" --param trigger_payload "{\"tags\": [\"cloudant\",\"ibm-cloudant\",\"pixiedust\", \"dashdb\"]}"
 wsk rule update so_couchdbrule so_couchdbish stackoverflow/socron
 wsk rule enable so_couchdbrule
 
 wsk trigger delete so_composedbs
-wsk trigger create so_composedbs --feed /whisk.system/alarms/alarm --param cron "8 * * * *" --param trigger_payload "{\"tags\": [\"postgresql\", \"redis\", \"scylladb\", \"etcd\", \"rabbitmq\"]}"
-wsk rule update so_composerule so_composedbs stackoverflow/socron
-wsk rule enable so_composerule
+wsk rule delete so_composerule
 
 wsk trigger delete so_moredbs
-wsk trigger create so_moredbs --feed /whisk.system/alarms/alarm --param cron "38 * * * *" --param trigger_payload "{\"tags\": [\"spark\", \"apache-spark\", \"dashdb\"]}"
-wsk rule update so_morerule so_moredbs stackoverflow/socron
-wsk rule enable so_morerule
+wsk rule delete so_morerule
 
 
