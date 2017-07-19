8e73dec8045f814c39e441303d21ff323b0a7121
Lorna Jane Mitchell <lorna@lornajane.net>
2017-07-19 11:31:52 +0100

    Enable toggling slack and bot notifications on and off

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -2,31 +2,37 @@ function main(data) {
   return new Promise(function(resolve, reject) {
     var request = require('request');
 
-    if(data.status == 'new') {
-      var message = "New question: <" + data.question.link + "|" + data.question.title + "> (tagged: " + data.question.tags + ")";
-      console.log(message);
+    // primitive feature toggles to transition between notification types
+    var send_to_slack = true;
+    var send_to_hubot = true;
 
-      var options = {
+    if(data.status == 'new') {
+      if(send_to_slack) {
+        var message = "New question: <" + data.question.link + "|" + data.question.title + "> (tagged: " + data.question.tags + ")";
+        console.log(message);
+
+        var options = {
           "text": message,
           "icon_emoji": ":postit:"
-      };
+        };
 
-      request({
-        url: data.slackURL,
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(options)
-      }, function (err, response, body) {
-        if(err) {
-          console.log(err);
-          reject ({payload: "Failed"});
-        } else {
-          console.log("Status: " + response.statusCode);
-          console.log("Response Body: " + body);
-          resolve( {payload: "Notified"} );
-        }
-      });
+        request({
+          url: data.slackURL,
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(options)
+        }, function (err, response, body) {
+          if(err) {
+            console.log(err);
+            reject ({payload: "Failed"});
+          } else {
+            console.log("Status: " + response.statusCode);
+            console.log("Response Body: " + body);
+            resolve( {payload: "Notified"} );
+          }
+        });
 
+      }
     } else {
       resolve( {payload: "Complete"} );
     }
