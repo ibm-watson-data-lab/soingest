965b1d6f99b4648d11fef31381aa15dab396b363
Lorna Jane Mitchell <lorna@lornajane.net>
2017-07-31 21:12:01 +0100

    Add a new rule to get questions tagged compose, less frequently

--- rules.sh
+++ rules.sh
@@ -7,3 +7,8 @@ wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron 
 wsk rule update so_couchdbrule so_couchdbish stackoverflow/socron
 wsk rule enable so_couchdbrule
 
+wsk trigger delete so_compose
+wsk trigger create so_compose --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"compose\"]}"
+wsk rule update so_composerule so_compose stackoverflow/socron
+wsk rule enable so_composerule
+
