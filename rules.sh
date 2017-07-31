a59e3dc8d4f2d0c865d8c016ffa1fe076d83dbf5
Lorna Jane Mitchell <lorna@lornajane.net>
2017-07-31 11:58:24 +0100

    Remove DashDB tag

--- rules.sh
+++ rules.sh
@@ -3,14 +3,7 @@
 # This sets up the rules and triggers to get our periodic triggers working
 
 wsk trigger delete so_couchdbish
-wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron "*/5 * * * *" --param trigger_payload "{\"tags\": [\"cloudant\",\"ibm-cloudant\",\"pixiedust\", \"dashdb\"]}"
+wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron "*/5 * * * *" --param trigger_payload "{\"tags\": [\"cloudant\",\"ibm-cloudant\",\"pixiedust\"]}"
 wsk rule update so_couchdbrule so_couchdbish stackoverflow/socron
 wsk rule enable so_couchdbrule
-
-wsk trigger delete so_composedbs
-wsk rule delete so_composerule
-
-wsk trigger delete so_moredbs
-wsk rule delete so_morerule
-
 
