6183ff6165b1105e56b995ca57d062057965bd8c
Lorna Jane Mitchell <lorna@lornajane.net>
2017-08-18 09:09:30 +0100

    Extend tag list for compose

--- rules.sh
+++ rules.sh
@@ -8,7 +8,7 @@ wsk rule update so_couchdbrule so_couchdbish stackoverflow/socron
 wsk rule enable so_couchdbrule
 
 wsk trigger delete so_compose
-wsk trigger create so_compose --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"compose\"]}"
+wsk trigger create so_compose --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"compose\", \"compose.io\"]}"
 wsk rule update so_composerule so_compose stackoverflow/socron
 wsk rule enable so_composerule
 
