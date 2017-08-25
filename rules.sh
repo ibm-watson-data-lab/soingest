d53f4ebd6418045755642cd9b902adfb4ffd8613
Lorna Jane Mitchell <lorna@lornajane.net>
2017-08-25 12:02:49 +0100

    Spread API calls out to avoid rate limits

--- rules.sh
+++ rules.sh
@@ -17,13 +17,13 @@
 # the AND searches each need their own entry
 # spark search
 ./wsk trigger delete so_spark
-./wsk trigger create so_spark --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"apache-spark\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
+./wsk trigger create so_spark --feed /whisk.system/alarms/alarm --param cron "2,22,42 * * * *" --param trigger_payload "{\"tags\": [\"apache-spark\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
 ./wsk rule update so_sparkrule so_spark stackoverflow/socron
 ./wsk rule enable so_sparkrule
 
 # juptyer search
 ./wsk trigger delete so_jupyter
-./wsk trigger create so_jupyter --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"jupyter-notebooks\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
+./wsk trigger create so_jupyter --feed /whisk.system/alarms/alarm --param cron "9,29,49 * * * *" --param trigger_payload "{\"tags\": [\"jupyter-notebooks\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
 ./wsk rule update so_jupyterrule so_jupyter stackoverflow/socron
 ./wsk rule enable so_jupyterrule
 
