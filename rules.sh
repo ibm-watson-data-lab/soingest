9da367531d79218557d5c62d926cb56c380872fd
Lorna Jane Mitchell <lorna@lornajane.net>
2017-11-22 08:58:39 +0000

    Use package name variable rather than hardcoded value

--- rules.sh
+++ rules.sh
@@ -5,25 +5,25 @@
 # Maxiumum of five tags per search
 ./Bluemix_CLI/bin/bluemix wsk trigger delete so_couchdbish
 ./Bluemix_CLI/bin/bluemix wsk trigger create so_couchdbish --feed /whisk.system/alarms/alarm --param cron "*/5 * * * *" --param trigger_payload "{\"tags\": [\"cloudant\",\"ibm-cloudant\",\"pixiedust\",\"data-science-experience\"]}"
-./Bluemix_CLI/bin/bluemix wsk rule update so_couchdbrule so_couchdbish stackoverflow/socron
+./Bluemix_CLI/bin/bluemix wsk rule update so_couchdbrule so_couchdbish $WSK_PACKAGE/socron
 ./Bluemix_CLI/bin/bluemix wsk rule enable so_couchdbrule
 
 # A less frequent search for compose
 ./Bluemix_CLI/bin/bluemix wsk trigger delete so_compose
 ./Bluemix_CLI/bin/bluemix wsk trigger create so_compose --feed /whisk.system/alarms/alarm --param cron "7,27,47 * * * *" --param trigger_payload "{\"tags\": [\"compose\", \"compose.io\"]}"
-./Bluemix_CLI/bin/bluemix wsk rule update so_composerule so_compose stackoverflow/socron
+./Bluemix_CLI/bin/bluemix wsk rule update so_composerule so_compose $WSK_PACKAGE/socron
 ./Bluemix_CLI/bin/bluemix wsk rule enable so_composerule
 
 # the AND searches each need their own entry
 # spark search
 ./Bluemix_CLI/bin/bluemix wsk trigger delete so_spark
 ./Bluemix_CLI/bin/bluemix wsk trigger create so_spark --feed /whisk.system/alarms/alarm --param cron "2,22,42 * * * *" --param trigger_payload "{\"tags\": [\"apache-spark\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
-./Bluemix_CLI/bin/bluemix wsk rule update so_sparkrule so_spark stackoverflow/socron
+./Bluemix_CLI/bin/bluemix wsk rule update so_sparkrule so_spark $WSK_PACKAGE/socron
 ./Bluemix_CLI/bin/bluemix wsk rule enable so_sparkrule
 
 # juptyer search
 ./Bluemix_CLI/bin/bluemix wsk trigger delete so_jupyter
 ./Bluemix_CLI/bin/bluemix wsk trigger create so_jupyter --feed /whisk.system/alarms/alarm --param cron "9,29,49 * * * *" --param trigger_payload "{\"tags\": [\"jupyter-notebooks\", \"ibm-bluemix\"], \"search_type\": \"and\"}"
-./Bluemix_CLI/bin/bluemix wsk rule update so_jupyterrule so_jupyter stackoverflow/socron
+./Bluemix_CLI/bin/bluemix wsk rule update so_jupyterrule so_jupyter $WSK_PACKAGE/socron
 ./Bluemix_CLI/bin/bluemix wsk rule enable so_jupyterrule
 
